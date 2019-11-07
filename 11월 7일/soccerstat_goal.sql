ff8dc8a1d81402d89dd9bf2b21c15ae3b1b32dfb
Sang-Gyu Park <mcmanaman7@naver.com>
2019-11-07 21:51:10 +0900

    11월 7일 DB

---
+++ 11월 7일/soccerstat_goal.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: soccerstat
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `goal`
+--
+
+DROP TABLE IF EXISTS `goal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `goal` (
+  `gNo` int(11) NOT NULL AUTO_INCREMENT,
+  `scheduleNo` int(11) NOT NULL,
+  `player` varchar(45) DEFAULT NULL,
+  `team` varchar(45) DEFAULT NULL,
+  `goalTime` int(11) NOT NULL,
+  `owned` varchar(5) NOT NULL DEFAULT 'N',
+  `pkApt` int(11) NOT NULL DEFAULT '0',
+  `pkScc` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`gNo`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `goal`
+--
+
+LOCK TABLES `goal` WRITE;
+/*!40000 ALTER TABLE `goal` DISABLE KEYS */;
+INSERT INTO `goal` VALUES (1,1,'마커스 래쉬포드','맨체스터 유나이티드',27,'N',0,0),(2,1,'앤써니 마샬','맨체스터 유나이티드',75,'N',0,0),(3,1,'제이미 바디','레스터 시티',35,'N',0,0),(4,2,'기성용','뉴캐슬',65,'N',0,0);
+/*!40000 ALTER TABLE `goal` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-07 21:50:12
