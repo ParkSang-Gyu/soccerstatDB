0c230b82558f3f7ab9a20c7b8da45976b064b848
Sang-Gyu Park <mcmanaman7@naver.com>
2019-11-29 21:49:22 +0900

    11월 29일 DB

---
+++ 11월 29일/soccerstat_matches.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: soccerstat
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `matches`
+--
+
+DROP TABLE IF EXISTS `matches`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `matches` (
+  `scheduleNo` int(11) NOT NULL,
+  `homePos` int(11) NOT NULL,
+  `awayPos` int(11) NOT NULL,
+  `homeSht` int(11) NOT NULL,
+  `awaySht` int(11) NOT NULL,
+  `homeOT` int(11) NOT NULL,
+  `awayOT` int(11) NOT NULL,
+  `homePs` double NOT NULL,
+  `awayPs` double NOT NULL,
+  `homeCorner` int(11) NOT NULL,
+  `awayCorner` int(11) NOT NULL,
+  PRIMARY KEY (`scheduleNo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matches`
+--
+
+LOCK TABLES `matches` WRITE;
+/*!40000 ALTER TABLE `matches` DISABLE KEYS */;
+INSERT INTO `matches` VALUES (1,55,45,10,12,4,4,78,79,7,6),(2,51,49,16,13,6,2,80,72,5,5),(3,52,48,9,15,1,3,75,75,4,4),(4,60,40,13,13,2,5,76,79,11,6),(5,45,55,7,10,3,4,72,79,3,12),(6,51,49,10,16,5,3,79,80,2,7),(7,61,39,20,5,7,0,76,74,10,2),(8,37,63,14,9,2,3,78,72,5,1),(9,43,57,23,10,10,3,81,71,4,6),(10,48,52,16,7,4,0,77,75,8,5),(11,50,50,7,12,0,5,75,76,3,9),(12,48,52,15,11,4,4,78,75,6,3);
+/*!40000 ALTER TABLE `matches` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-29 21:48:27
---
+++ 11월 29일/soccerstat_offensive.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: soccerstat
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `offensive`
+--
+
+DROP TABLE IF EXISTS `offensive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `offensive` (
+  `oNo` int(11) NOT NULL AUTO_INCREMENT,
+  `scheduleNo` int(11) NOT NULL,
+  `player` varchar(45) DEFAULT NULL,
+  `team` varchar(45) DEFAULT NULL,
+  `shootApt` int(11) NOT NULL,
+  `shootOT` int(11) NOT NULL,
+  `dribbleApt` int(11) NOT NULL,
+  `dribbleScc` int(11) NOT NULL,
+  `offside` int(11) NOT NULL,
+  `fouled` int(11) NOT NULL,
+  `rating` double NOT NULL,
+  `motm` varchar(5) NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`oNo`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `offensive`
+--
+
+LOCK TABLES `offensive` WRITE;
+/*!40000 ALTER TABLE `offensive` DISABLE KEYS */;
+INSERT INTO `offensive` VALUES (1,1,'마커스 래쉬포드','맨체스터 유나이티드',5,2,3,1,2,4,7,'Y'),(2,1,'제임스 매디슨','레스터 시티',3,1,5,3,0,5,8,'N'),(3,9,'사디오 마네','리버풀',3,2,3,2,1,4,9,'Y'),(4,1,'해리 매과이어','레스터 시티',1,0,2,1,0,1,7,'N');
+/*!40000 ALTER TABLE `offensive` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-29 21:48:29
