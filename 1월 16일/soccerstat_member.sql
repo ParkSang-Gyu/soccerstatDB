3dffaec947ec5b6bdaf9160014101e369310291d
Sang-Gyu Park <mcmanaman7@naver.com>
2020-01-16 18:02:58 +0900

    1월 16일 DB

---
+++ 1월 16일/soccerstat_member.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: soccerstat
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member` (
+  `id` varchar(45) NOT NULL,
+  `pw` varchar(255) NOT NULL,
+  `writer` varchar(45) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `hp` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `authority` varchar(45) NOT NULL DEFAULT 'USER',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member`
+--
+
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES ('aaaa1111','$2a$10$dEchbowqnTc4VK4hxLCRSeenk2rvbeWqzqNxiVi7duUE1dFZSW/zO','흥미니','손흥민','M','010-2222-3333','a@naver.com','USER'),('aaaa2222','$2a$10$bx9iUnFfZua0ZjZObPLGIejPJnGUS.I1B67EO.EJ7bJ/rLZAKPNgu','맥카','맥마나만','M','010-2222-4444','c@naver.com','USER'),('aaaa3333','$2a$10$rzeukQFfOPr/Lgg4RxxeiuOUC.zE2i6a7OXBnqqhTzt5fsPs4AuYC','포치','포체티노','M','010-2222-7777','g@naver.com','USER'),('aaaa5555','$2a$10$9KlJoBV57dI73VOSpgo2nOKSqXqvmp5W42xtbrktX0gKR/ZYV67PO','쿤아게로','아게로','M','010-1111-7777','e@naver.com','USER'),('mcmanaman7','$2a$10$uwEV1oN5vehnvoeqI0J0Uu71q8L8mvLfqAnIf5AdpN2OlfDDDHPiC','관리','관리자','M','010-7777-7777','m@naver.com','ADMIN');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-16 17:59:14
